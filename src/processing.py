9eb4f84bba44ccfd1a8704d89688b72c349610ae
Ksenia Veremyeva < >
2025-04-18 22:57:08 +0300

    Исправление домашней работы 10.1

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,7 @@
-from typing import List, Dict
+from typing import List, Dict, Any
 
-dict_list = [
+
+state_filter = [
     {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
@@ -8,15 +9,15 @@ dict_list = [
 ]
 
 
-def filter_by_state(dict_list: List[Dict], state: str = 'EXECUTED') -> List[Dict]:
-    """Функция выозвращает новый список, отфильтрованный по указанному значению"""
-    return [item for item in dict_list if item.get("state") == state]
+def filter_by_state(state_filter: List[Dict[str, Any]], state: str = 'EXECUTED') -> List[Dict[str, Any]]:
+    """Функция возвращает новый список, отфильтрованный по указанному значению"""
+    return [item for item in state_filter if item.get("state") == state]
 
 
-print(filter_by_state(dict_list, "EXECUTED"))
+print(filter_by_state(state_filter, "EXECUTED"))
 
 
-data_list = [
+sort_data = [
             {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
             {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
             {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
@@ -24,9 +25,9 @@ data_list = [
         ]
 
 
-def sort_by_date(data_list: list, reverse: bool = True) -> list:
-    """Функция должна возвращает новый список, отсортированный по дате"""
-    return sorted(data_list, key=lambda x: x.get("date"), reverse=reverse)
+def sort_by_date(sort_data: List[Dict[str, Any]], reverse: bool = True) -> List[Dict[str, Any]]:
+    """Функция должна возвращать новый список, отсортированный по дате"""
+    return sorted(sort_data, key=lambda x: str(x.get("date")), reverse=reverse)
 
 
-print(sort_by_date(data_list))
+print(sort_by_date(sort_data))
